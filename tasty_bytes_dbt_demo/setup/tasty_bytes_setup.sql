19d3b96127c549195058182a950e56da4c169b33
mhaniffajari <muhammad.hanif81@ui.ac.id>
2026-04-25 06:53:10 +0000

    dbt run

--- tasty_bytes_dbt_demo/setup/tasty_bytes_setup.sql
+++ tasty_bytes_dbt_demo/setup/tasty_bytes_setup.sql
@@ -102,19 +102,19 @@ CREATE OR REPLACE API INTEGRATION tb_dbt_git_api_integration
 -- =============================================================================
 
 -- Create NETWORK RULE for external access integration
--- CREATE OR REPLACE NETWORK RULE dbt_network_rule
---   MODE = EGRESS
---   TYPE = HOST_PORT
---   -- Minimal URL allowlist that is required for dbt deps
---   VALUE_LIST = (
---     'hub.getdbt.com',
---     'codeload.github.com'
---     );
+CREATE OR REPLACE NETWORK RULE dbt_network_rule
+  MODE = EGRESS
+  TYPE = HOST_PORT
+  -- Minimal URL allowlist that is required for dbt deps
+  VALUE_LIST = (
+    'hub.getdbt.com',
+    'codeload.github.com'
+    );
 
 -- Create EXTERNAL ACCESS INTEGRATION for dbt access to external dbt package locations
--- CREATE OR REPLACE EXTERNAL ACCESS INTEGRATION dbt_ext_access
---   ALLOWED_NETWORK_RULES = (dbt_network_rule)
---   ENABLED = TRUE;
+CREATE OR REPLACE EXTERNAL ACCESS INTEGRATION dbt_ext_access
+  ALLOWED_NETWORK_RULES = (dbt_network_rule)
+  ENABLED = TRUE;
 
 -- =============================================================================
 -- STEP 6: Set up source data - Tasty Bytes foundational data model
